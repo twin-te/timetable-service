ec7c0334721627203cc45b86a712103d6bad59b3
SIY1121 <sota.0512.0213@gmail.com>
2021-02-09 13:14:15 +0000

    update: grpc error handling

--- src/grpc/timetable.service.ts
+++ src/grpc/timetable.service.ts
@@ -15,111 +15,113 @@ import tagRepository from '../database/tagRepository'
 import registeredCourseRepository from '../database/registeredCourseRepository'
 import { entityToGrpcCourse, grpcCourseToEntity } from './converter'
 import { Status } from '@grpc/grpc-js/build/src/constants'
-import { sendUnaryData } from '@grpc/grpc-js'
-
-function handleError(callback: sendUnaryData<any>) {
-  return (reason: any) => {
-    callback(reason)
-  }
-}
 
 /**
  * TimetableServiceの実装
  */
 export const timetableService: GrpcServer<TimetableService> = {
-  getRegisteredCourses({ request }, callback) {
-    registeredCourseRepository
-      .read(request.userId, request.year)
-      .then((courses) => {
+  async getRegisteredCourses({ request }, callback) {
+    try {
+      const courses = await registeredCourseRepository.read(
+        request.userId,
+        request.year
+      )
+      callback(
+        null,
+        GetRegisteredCoursesResponse.create({
+          courses: entityToGrpcCourse(courses),
+        })
+      )
+    } catch (e) {
+      callback(e)
+    }
+  },
+  async getTags({ request }, callback) {
+    try {
+      const tags = await tagRepository.read(request.userId)
+      callback(null, GetTagsResponse.create({ tags }))
+    } catch (e) {
+      callback(e)
+    }
+  },
+  async createRegisteredCourses({ request }, callback) {
+    try {
+      const courses = await registeredCourseRepository.create(
+        grpcCourseToEntity(request.courses)
+      )
+      callback(
+        null,
+        CreateRegisterCoursesResponse.create({
+          courses: entityToGrpcCourse(courses),
+        })
+      )
+    } catch (e) {
+      callback(e)
+    }
+  },
+  async createTags({ request }, callback) {
+    try {
+      const tags = await tagRepository.create(
+        request.tags.map((t) => ({ ...t, id: v4() }))
+      )
+      callback(
+        null,
+        CreateTagsResponse.create({
+          tags,
+        })
+      )
+    } catch (e) {
+      callback(e)
+    }
+  },
+  async updateRegisteredCourses({ request }, callback) {
+    try {
+      const courses = await registeredCourseRepository.update(
+        grpcCourseToEntity(request.courses)
+      )
+      if (!courses) callback({ code: Status.NOT_FOUND })
+      else
         callback(
           null,
-          GetRegisteredCoursesResponse.create({
+          UpdateRegisteredCoursesResponse.create({
             courses: entityToGrpcCourse(courses),
           })
         )
-      })
-      .catch(handleError(callback))
+    } catch (e) {
+      callback(e)
+    }
   },
-  getTags({ request }, callback) {
-    tagRepository
-      .read(request.userId)
-      .then((tags) => {
-        callback(null, GetTagsResponse.create({ tags }))
-      })
-      .catch(handleError(callback))
-  },
-  createRegisteredCourses({ request }, callback) {
-    registeredCourseRepository
-      .create(grpcCourseToEntity(request.courses))
-      .then((courses) => {
+  async updateTags({ request }, callback) {
+    try {
+      const tags = await tagRepository.update(request.tags)
+      if (!tags) callback({ code: Status.NOT_FOUND })
+      else
         callback(
           null,
-          CreateRegisterCoursesResponse.create({
-            courses: entityToGrpcCourse(courses),
-          })
-        )
-      })
-      .catch(handleError(callback))
-  },
-  createTags({ request }, callback) {
-    tagRepository
-      .create(request.tags.map((t) => ({ ...t, id: v4() })))
-      .then((tags) => {
-        callback(
-          null,
-          CreateTagsResponse.create({
+          UpdateTagsResponse.create({
             tags,
           })
         )
-      })
-      .catch(handleError(callback))
+    } catch (e) {
+      callback(e)
+    }
   },
-  updateRegisteredCourses({ request }, callback) {
-    registeredCourseRepository
-      .update(grpcCourseToEntity(request.courses))
-      .then((courses) => {
-        if (!courses) callback({ code: Status.NOT_FOUND })
-        else
-          callback(
-            null,
-            UpdateRegisteredCoursesResponse.create({
-              courses: entityToGrpcCourse(courses),
-            })
-          )
-      })
-      .catch(handleError(callback))
+  async deleteRegisteredCourses({ request }, callback) {
+    try {
+      const success = await registeredCourseRepository.delete(request.ids)
+      if (!success) callback({ code: Status.NOT_FOUND })
+      else callback(null, DeleteRegisteredCoursesResponse.create({}))
+    } catch (e) {
+      callback(e)
+    }
   },
-  updateTags({ request }, callback) {
-    tagRepository
-      .update(request.tags)
-      .then((tags) => {
-        if (!tags) callback({ code: Status.NOT_FOUND })
-        else
-          callback(
-            null,
-            UpdateTagsResponse.create({
-              tags,
-            })
-          )
-      })
-      .catch(handleError(callback))
-  },
-  deleteRegisteredCourses({ request }, callback) {
-    registeredCourseRepository
-      .delete(request.ids)
-      .then((success) => {
-        if (!success) callback({ code: Status.NOT_FOUND })
-        else callback(null, DeleteRegisteredCoursesResponse.create({}))
-      })
-      .catch(handleError(callback))
-  },
-  deleteTags({ request }, callback) {
-    tagRepository
-      .delete(request.ids)
-      .then((success) => {
-        if (!success) callback({ code: Status.NOT_FOUND })
-        else callback(null, DeleteTagsResponse.create({}))
-      })
-      .catch(handleError(callback))
+  async deleteTags({ request }, callback) {
+    try {
+      const success = await tagRepository.delete(request.ids)
+      if (!success) callback({ code: Status.NOT_FOUND })
+      else callback(null, DeleteTagsResponse.create({}))
+    } catch (e) {
+      callback(e)
+    }
   },
 }
